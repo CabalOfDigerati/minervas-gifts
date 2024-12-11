b32b1961f5188fea9a00aaa5eb5a8c6c04c6e3d1
Digerati <cabal@digerati.design>
2024-12-11 13:27:30 +0000

    Disable Gift Variant fields when not selected in order to clean up POST data for client

--- src/minerva/displayGiftVariants.ts
+++ src/minerva/displayGiftVariants.ts
@@ -13,12 +13,27 @@ export const displayGiftVariants = () => {
             const allGiftVariants = document.querySelectorAll('[dd-gift="variants"]');
             allGiftVariants.forEach(variant => {
                 variant.classList.add('hide');
+                let variantRadioButtons = variant.querySelectorAll('input[type="radio"]');
+                console.log(variantRadioButtons);
+                if (!variantRadioButtons) {
+                    return;
+                }
+                variantRadioButtons.forEach(variantRadioButton => {
+                    variantRadioButton.setAttribute('disabled', 'disabled');
+                });
             });
             let giftVariants = gift.querySelector('[dd-gift="variants"]');
             if (!giftVariants) {
                 return;
             }
             giftVariants.classList.remove('hide');
+            let giftVariantRadioButtons = giftVariants.querySelectorAll('input[type="radio"]');
+            if (!giftVariantRadioButtons) {
+                return;
+            }
+            giftVariantRadioButtons.forEach(giftVariantRadioButton => {
+                giftVariantRadioButton.removeAttribute('disabled');
+            });
         });
     });
 };
